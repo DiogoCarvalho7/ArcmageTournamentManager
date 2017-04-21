b80db97c126de1fe2e024373d9a257f3222760e5
DiogoCarvalho7 <dcc.mkt@gmail.com>
2017-04-21 09:38:39 +0100

    Add files via upload

--- AMT/Form1.Designer.cs
+++ AMT/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.login_button = new System.Windows.Forms.Button();
+            this.user = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.user_tb = new System.Windows.Forms.TextBox();
+            this.pass_tb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(106, 154);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(75, 23);
+            this.login_button.TabIndex = 0;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Location = new System.Drawing.Point(60, 59);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(29, 13);
+            this.user.TabIndex = 1;
+            this.user.Text = "User";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(60, 95);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(53, 13);
+            this.pass.TabIndex = 2;
+            this.pass.Text = "Password";
+            // 
+            // user_tb
+            // 
+            this.user_tb.Location = new System.Drawing.Point(122, 52);
+            this.user_tb.Name = "user_tb";
+            this.user_tb.Size = new System.Drawing.Size(100, 20);
+            this.user_tb.TabIndex = 3;
+            // 
+            // pass_tb
+            // 
+            this.pass_tb.Location = new System.Drawing.Point(122, 95);
+            this.pass_tb.Name = "pass_tb";
+            this.pass_tb.Size = new System.Drawing.Size(100, 20);
+            this.pass_tb.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 203);
+            this.Controls.Add(this.pass_tb);
+            this.Controls.Add(this.user_tb);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.login_button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label user;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.TextBox user_tb;
+        private System.Windows.Forms.TextBox pass_tb;
     }
 }
 
